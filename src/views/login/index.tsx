a0c2fc298b500ab9830f70d3a74ed222bcd93c42
OctavioCecio <octaviocecio@gmail.com>
2024-09-26 20:56:38 -0300

    add: Password Lock and styles

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,13 +1,21 @@
 import React, { FormEvent, ReactElement, useState } from "react";
-import "./styles.scss";
 import handlePromise from "../../utils/promise";
 import useAuthService from "../../services/auth.service";
-import { Button, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import './styles.scss';
+import InputAdornment from "@mui/material/InputAdornment";
+import EmailIcon from "@mui/icons-material/Email";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton'; 
 
 export default function Login(): ReactElement {
   const { login } = useAuthService();
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const onLogin = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
@@ -19,48 +27,108 @@ export default function Login(): ReactElement {
     navigate("/home");
   };
 
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  height: '100vh',
-};
+  const logoUniStyle = {
+    textAlign: 'center',
+  };
 
-const bannerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  backgroundColor: '#2b76ff',
-};
+  const imageStyle = {
+    zIndex: 10,
+    position: 'relative',
+  };
 
-const logoUniStyle = {
-  textAlign: 'center',
-};
+  const formEndStyle = {
+    backgroundColor: 'white',
+    height: '100vh',
+    width: '100vw',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 8,
+    border: 'radius 20px', // revisar no me esta funcionando
+  };
 
-const buttonStyle = {
-  textTransform: 'none',
-};
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '100vh'
+  };
 
+  const bannerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#2b76ff',
+    width: '100vw'
+  };
+
+  const textFieldStyle = {
+    width : '90vw',
+    marginTop: '40px', 
+  }
+
+
+  const buttonStyle = {
+    borderRadius: '8px',
+    textTransform: 'none',
+    backgroundColor : '#2b76ff',
+    padding: '12px',
+    color: 'white',
+    boxShadow: 'none', // revisar 
+    width : '90vw',
+    marginTop: '20px', 
+
+  };
 
   return (
     <div className="container" style={containerStyle}>
       <div className="banner" style={bannerStyle}>
-        <div className="logo-uni" style={logoUniStyle}>
-          <img src="/image/logo-universidad.png" alt="Login image"/>
-        </div>
-        <div className="image">
-          <img src="/image/loginImage.png" alt="Login Image" />
-        </div>
+        <img src="/image/logo-universidad.png" alt="Login image" style={logoUniStyle} />
+        <img src="/image/loginImage.png" alt="Login Image" style={imageStyle} />
       </div>
-      <div className="form">
-        <form onSubmit={onLogin}>
-          <TextField variant="filled" type="text" label="Email" name="email" />
-          <TextField variant="filled" type="password" label="Contraseña" name="password" />
-          <Button type="submit" style={buttonStyle}>Iniciar Sesión </Button>
+        <form onSubmit={onLogin} style={formEndStyle}>
+          <TextField style={textFieldStyle} 
+            variant='filled' 
+            placeholder="Email" 
+            type="text" 
+            name="email" 
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField  style={textFieldStyle}
+            variant="filled" 
+            type={showPassword ? "text" : "password"} // Cambiar según el estado de showPassword
+            placeholder="Contraseña" 
+            name="password" 
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleTogglePasswordVisibility}
+                    onMouseDown={(e) => e.preventDefault()} // Prevenir el comportamiento predeterminado
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />} {/* Mostrar ícono según el estado */}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button type="submit" style={buttonStyle}  >Iniciar Sesión</Button>
           {error && <small>{error}</small>}
         </form>
-      </div>
     </div>
   );
 }
