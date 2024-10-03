c61c6efc64f38f64b18c21babaf6e91f213a43fa
Lucas Julian Guerci <lucas.guerci@gmail.com>
2024-10-02 21:39:30 -0300

    feature: implemented nav component and error views

--- src/components/mobile-nav/index.tsx
+++ src/components/mobile-nav/index.tsx
@@ -1,5 +1,28 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useLayoutEffect, useState } from "react";
+import { useAuth } from "../../context/auth.context";
+import NavButton, { NavButtonProps } from "./button";
+import { NAV_BUTTONS } from "./const/button-const";
+import "./styles.scss";
+
+export type NavButtonType = NavButtonProps & {
+  role: string;
+};
 
 export default function MobileNav(): ReactElement {
-  return <nav></nav>;
+  const [userButtons, setUserButtons] = useState<NavButtonType[]>([]);
+  const authService = useAuth();
+
+  useLayoutEffect(() => {
+    const authToken = authService.getTokenInfo();
+    if (!authToken) return;
+    setUserButtons(NAV_BUTTONS.filter((btn) => btn.role === "any" || authToken.role.includes(btn.role)));
+  }, []);
+
+  return (
+    <nav className="mobile">
+      {userButtons.map((btn, i) => (
+        <NavButton {...btn} key={i} />
+      ))}
+    </nav>
+  );
 }
--- src/views/template/index.tsx
+++ src/views/template/index.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import MobileNav from "../../components/mobile-nav";
 import Header from "../../components/header";
-import { AuthProvider } from "../../context/auth.context";
 
 export default function Template() {
   return (
-    <AuthProvider>
+    <>
       <Header />
       <main>
         <Outlet />
       </main>
       <MobileNav />
-    </AuthProvider>
+    </>
   );
 }
