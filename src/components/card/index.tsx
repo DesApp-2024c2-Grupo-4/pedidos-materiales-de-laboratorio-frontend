b07b8b004f1bd7a82b43fd0b049b1acc1f6bedf0
cinthia <cinthiapardos@gmail.com>
2024-10-23 22:43:53 -0300

    feat: add css and test for review

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -29,26 +29,28 @@ export default function CardRequest({
   //   navigate(-1);
   // };
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="card-header">
-          <div>
-            <h3 className="card-title">{title}</h3>
-            <p>Fecha practica: {date}</p>
+    <Link to="/requests">
+      <div className="card">
+        <div className="card-body">
+          <div className="card-header">
+            <div>
+              <h3 className="card-title">{title}</h3>
+              <p>Fecha practica: {date}</p>
+            </div>
+            {banner && <p className={`card-banner ${banner}`}>{banner}</p>}
           </div>
-          {banner && <p className={`card-banner ${banner}`}>{banner}</p>}
-        </div>
-        <div className="card-info">
-          <div>
-            <p>Laboratorio: {laboratory}</p>
-            <p>Edificio: {building}</p>
-          </div>
-          <div>
-            <p>Profesor: {proffesor}</p>
-            <p>Estudiantes: {students}</p>
+          <div className="card-info">
+            <div>
+              <p>Laboratorio: {laboratory}</p>
+              <p>Edificio: {building}</p>
+            </div>
+            <div>
+              <p>Profesor: {proffesor}</p>
+              <p>Estudiantes: {students}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
